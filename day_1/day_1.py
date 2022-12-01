25c2a5370f7636c95dfc3dc845d60d65e1b2acbc
Holly Fox <fox.holly.13@gmail.com>
2022-12-01 10:05:01 -0500

    day 1 puzzle 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -19,4 +19,30 @@ class Day1(ElfMachine):
         return most_calories
 
     def solve_second_puzzle(self, input_data: str) -> str:
-        raise NotImplementedError
+        # Re-add an empty string so we can check last elf's calories
+        calories = [*input_data.split("\n"), ""]
+
+        most_calories = 0
+        second_most_calories = 0
+        third_most_calories = 0
+        current_elf_count = 0
+
+        for item in calories:
+            if item == "":
+                if current_elf_count > most_calories:
+                    second_most_calories = most_calories
+                    most_calories = current_elf_count
+
+                elif current_elf_count > second_most_calories:
+                    third_most_calories = second_most_calories
+                    second_most_calories = current_elf_count
+
+                elif current_elf_count > third_most_calories:
+                    third_most_calories = current_elf_count
+
+                current_elf_count = 0
+                continue
+
+            current_elf_count += int(item)
+
+        return most_calories + second_most_calories + third_most_calories
