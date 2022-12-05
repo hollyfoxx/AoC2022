6e440a9d684b22a31db2a6f6667976104fd2b9ec
Holly Fox <fox.holly.13@gmail.com>
2022-12-04 20:21:34 -0500

    Update test runner for proper python install

--- bin/run_tests.bat
+++ bin/run_tests.bat
@@ -1,5 +1,5 @@
 @echo off 
 
 set test_id=%1
-if not "%test_id%" == "" (python3 -m pytest .\test.py -s -vv -k %test_id%)
-if "%test_id%" == "" (python3 -m pytest .\test.py -s -vv)
+if not "%test_id%" == "" (pytest .\test.py -s -vv -k %test_id%)
+if "%test_id%" == "" (pytest .\test.py -s -vv)
