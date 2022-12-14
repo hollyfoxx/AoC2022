f79542ed806a7e4d36783fbd93e2f696ae982163
Holly Fox <fox.holly.13@gmail.com>
2022-12-13 22:45:17 -0500

    json load

--- day_13/machine.py
+++ day_13/machine.py
@@ -1,9 +1,16 @@
+import json
 from elf_machine import ElfMachine
 
 
 class Day13(ElfMachine):
     def solve_first_puzzle(self, input_data: str) -> int:
-        return False
+        pairs = input_data.split("\n\n")
+
+        correct_pair_indices = []
+        for index, pair in enumerate(pairs):
+            left, right = pair.split("\n")
+            left = json.loads(left)
+            right = json.loads(right)
 
     def solve_second_puzzle(self, input_data: str) -> int:
         return False
