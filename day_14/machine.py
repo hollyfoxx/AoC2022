52cf64425ff7b71028b2cba22277b077d06301d8
Holly Fox <fox.holly.13@gmail.com>
2022-12-14 20:58:59 -0500

    Day 14 init

---
+++ day_14/machine.py
@@ -0,0 +1,9 @@
+from elf_machine import ElfMachine
+
+
+class Day14(ElfMachine):
+    def solve_first_puzzle(self, input_data: str) -> int:
+        return False
+
+    def solve_second_puzzle(self, input_data: str) -> int:
+        return False
--- test.py
+++ test.py
@@ -17,6 +17,62 @@ from day_10.machine import Day10
 from day_11.machine import Day11
 from day_12.machine import Day12
 from day_13.machine import Day13
+from day_14.machine import Day14
+
+
+@pytest.mark.parametrize(
+    "input_path,puzzle_number,machine,expected",
+    [
+        pytest.param(
+            os.path.join("day_14", "example.txt"),
+            1,
+            Day14,
+            13,
+            id="example_1",
+        ),
+        # pytest.param(
+        #     os.path.join("day_14", "puzzle.txt"),
+        #     1,
+        #     Day14,
+        #     6101,
+        #     id="puzzle_1",
+        # ),
+        # pytest.param(
+        #     os.path.join("day_14", "example.txt"),
+        #     2,
+        #     Day14,
+        #     140,
+        #     id="example_2",
+        # ),
+        # pytest.param(
+        #     os.path.join("day_14", "puzzle.txt"),
+        #     2,
+        #     Day14,
+        #     21909,
+        #     id="puzzle_2",
+        # ),
+    ],
+)
+def test_day_14(
+    input_path: str,
+    puzzle_number: int,
+    machine: ElfMachine,
+    expected: Union[int, str],
+):
+    machine = machine()
+    test_input = machine.read_input(path=input_path)
+
+    if puzzle_number == 1:
+        result = machine.solve_first_puzzle(test_input)
+
+    if puzzle_number == 2:
+        result = machine.solve_second_puzzle(test_input)
+
+    if expected:
+        assert result == expected
+        print(f"\nResult: {result}")
+    else:
+        print(f"\n(WIP) Result: {result}")
 
 
 @pytest.mark.parametrize(
